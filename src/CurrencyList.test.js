b57fb2a9e353c88b6e59ce277b50c69717d4628c
vtaneski <taneski.viktor@gmail.com>
2018-10-08 23:02:00 +0200

    basic test

--- src/CurrencyList.test.js
+++ src/CurrencyList.test.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CurrencyList from './CurrencyList';
 import CurrencyState from './CurrencyState';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+Enzyme.configure({ adapter: new Adapter() })
+
+const currencyState = new CurrencyState();
+    currencyState.fetchData();
 
 it('renders without crashing', () => {
-    const currencyState = new CurrencyState();
-    currencyState.fetchData();
+    
 
     const div = document.createElement('div');
     ReactDOM.render(<CurrencyList listState={currencyState} />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('Currency table', () => {
+    
+    it('CurrencyList renders table', () => {
+        const currencyList = shallow(<CurrencyList listState={currencyState} />);
+        expect(currencyList.find('table').length).toEqual(1);
+    });
 });
