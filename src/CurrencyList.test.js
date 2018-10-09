06b843abd974862ac8de18dacd21ac9bb673b5cf
Viktor <taneski.viktor@gmail.com>
2018-10-09 13:51:52 +0200

    CurrencyList test

--- src/CurrencyList.test.js
+++ src/CurrencyList.test.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CurrencyList from './CurrencyList';
+import CurrencyItem from './CurrencyList';
 import CurrencyState from './CurrencyState';
-import { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
-Enzyme.configure({ adapter: new Adapter() })
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, mount, configure } from 'enzyme';
+
+configure({ adapter: new Adapter() });
 
 const currencyState = new CurrencyState();
-    currencyState.fetchData();
+currencyState.fetchData();
 
 it('renders without crashing', () => {
-    
-
     const div = document.createElement('div');
     ReactDOM.render(<CurrencyList listState={currencyState} />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
-describe('Currency table', () => {
-    
-    it('CurrencyList renders table', () => {
-        const currencyList = shallow(<CurrencyList listState={currencyState} />);
+describe('CurrencyList', () => {
+    it('renders table', () => {
+        const currencyList = shallow(<CurrencyList listState={currencyState}/>);
         expect(currencyList.find('table').length).toEqual(1);
+    });
+
+    it('renders nested components', () => {
+        const currencyList = mount(<CurrencyList listState={currencyState}/>);
+        expect(currencyList.find('CurrencyItem').length).toEqual(100);
     });
 });
